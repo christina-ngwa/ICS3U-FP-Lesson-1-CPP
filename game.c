97a1531737b943c2f6d3e7c2df44746e736e94b2
Ubuntu <ubuntu@ip-172-31-20-42.ec2.internal>
2019-09-20 18:11:59 +0000

    hi

---
+++ game.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 Christina Ngwa All rights reserved.
+//
+// Created by: Christina Ngwa
+// Created on: September 2019
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+printf("Hello, World!");
+}
